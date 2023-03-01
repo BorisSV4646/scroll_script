4435343a5ac4cb64dde504306016572365f15fb4
Boris Vinnik <vinniklawyer@yandex.ru>
2023-03-01 20:52:57 +0800

    Done first transaction

--- My_script.py
+++ My_script.py
@@ -1,76 +1,93 @@
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
 from loguru import logger
+import time
 
-# logger.add("log/file_{time}.log",
-#            format="{time} {level} {message}", level="DEBUG")
+# WATH https://habr.com/ru/post/674204/ fot web3 on python
+# https://translated.turbopages.org/proxy_u/en-ru.ru.1526aeb4-63f6e7ac-aeec75a2-74722d776562/https/docs.soliditylang.org/en/develop/abi-spec.html - расшифровка
+# https://github.com/h1rdr3v2/faucetbch/blob/master/main.py - скрипт на кошелек
+# https://github.com/IgorGemsCodeAutomation/Arbitrum-warming_up_wallets/blob/main/main.py - проект парня из нашего чата
+# https://louisabraham.github.io/articles/no-abi.html - статья как отсылать без ABi на контракт
+# https://eth-converter.com/ - конвентер эфиры в гвеи и т.д.
 
 private_key = "3b58db09af4c2d51507cbb11cf902f50ae2902b9447478fdaff9226f1a646d5f"
 account_from = "0x7A8463B955c403ebd4611b18743a40aeBa0d7d4F"
-account_to = "0x2e2C1C959E6Ff27DbA3b7901ef86bFEA0174F0C1"
-# *WATH https://habr.com/ru/post/674204/ fot web3 on python
-# *https://l1scan.scroll.io/tx/0xdaf9e1855af2132eb1defbe766ab9eb855abd79f21817baa6a404017c0d267c0 - explorer
-# *https://translated.turbopages.org/proxy_u/en-ru.ru.1526aeb4-63f6e7ac-aeec75a2-74722d776562/https/docs.soliditylang.org/en/develop/abi-spec.html - расшифровка
-# *https://github.com/h1rdr3v2/faucetbch/blob/master/main.py - скрипт на кошелек
+
+scroll_alpha = Web3.HTTPProvider('https://alpha-rpc.scroll.io/l2')
+goerly = Web3.HTTPProvider(
+    'https://endpoints.omniatech.io/v1/eth/goerli/public')
 
 
-def get_wallet():
-    with open('wallets/wallet.txt', 'r') as file:
-        _main_wallet = [row.strip() for row in file]
-    return _main_wallet
-
-
-scroll_l1 = Web3.HTTPProvider('https://prealpha-rpc.scroll.io/l1')
-scroll_l2 = Web3.HTTPProvider('https://prealpha-rpc.scroll.io/l2')
-
-
-# web3 = Web3(scroll_l2)
-# logger.info(f"Is connected: {web3.isConnected()}")
+web3 = Web3(goerly)
+logger.info(f"Is connected: {web3.isConnected()}")
 # web3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-web3 = Web3(scroll_l1)
-logger.info(f"Is connected: {web3.isConnected()}")
-web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+def get_wallet_edge():
+    with open('wallets/wallet_edge.txt', 'r') as file:
+        _main_wallet = [row.strip() for row in file]
+        return _main_wallet
 
 
 def check_balance(_main_wallet):
-    for wallets in _main_wallet:
-        if ((web3.fromWei(web3.eth.get_balance(wallets), 'ether')) == 0):
-            logger.error(f'Balance {wallets} 0 ')
+    return (web3.fromWei(web3.eth.get_balance(_main_wallet), 'ether'))
+
+# *Разобраться с nonce - как его увеличить на 1 без while
 
 
-def send_txn(account_from, account_to):
-    amount = 0.01
+def send_txn(account_from):
+    wallets = get_wallet_edge()
+    for i in range(len(wallets)):
+        if (check_balance(wallets[i]) >= 0.03):
+            logger.info(f"{wallets[i]} have more than 0.01 eth")
+            continue
+        else:
+            amount = 0.01
+            gas = 2_000_000
+            nonce = web3.eth.getTransactionCount(account_from)
+            print(nonce)
+            txn = {
+                'chainId': 5,
+                'from': account_from,
+                'to': wallets[i],
+                'value': int(Web3.toWei(amount, 'ether')),
+                'nonce': nonce,
+                'gasPrice': web3.eth.gas_price,
+                'gas': gas,
+            }
+            signed_txn = web3.eth.account.sign_transaction(
+                txn, private_key)
+            txn_hash = web3.eth.sendRawTransaction(signed_txn.rawTransaction)
+            hash = txn_hash.hex()
+            logger.info(hash)
+            while (nonce == (web3.eth.getTransactionCount(account_from))):
+                time.sleep(3)
+            # txn = web3.eth.get_transaction_receipt(hash)
+            # logger.info(txn[0]['status'])
+
+# *Сделать цикл для разных кошелей. Разобраться как брат только мультипликатор функции
+# *и подставлять к нему закодированные данные
+
+
+def depositETH():
+    contract = "0xe5E30E7c24e4dFcb281A682562E53154C15D3332"
+    amount = 10000040
     gas = 2_000_000
-    nonce = web3.eth.getTransactionCount(account_from)
-    txn = {
-        'chainId': 534351,
+    dict_transaction = {
+        'chainId': 5,
         'from': account_from,
-        'to': account_to,
-        'value': int(Web3.toWei(amount, 'ether')),
-        'nonce': nonce,
+        'to': contract,
         'gasPrice': web3.eth.gas_price,
-        'gas': gas,
+        'nonce': web3.eth.getTransactionCount(account_from),
+        'value': int(Web3.toWei(amount, 'gwei')),
+        'data': "0x9f8420b3000000000000000000000000000000000000000000000000002386f26fc100000000000000000000000000000000000000000000000000000000000000009c40",
+        'gas': gas * 2,
     }
-
-    return txn
-
-
-transaction = send_txn(account_from, account_to)
-
-
-# 2. Подписываем транзакцию с приватным ключом
-signed_txn = web3.eth.account.sign_transaction(transaction, private_key)
+    signed_txn = web3.eth.account.sign_transaction(
+        dict_transaction, private_key)
+    txn_hash = web3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    print(txn_hash.hex())
 
 
-# 3. Отправка транзакции
-txn_hash = web3.eth.sendRawTransaction(signed_txn.rawTransaction)
-
-# Получаем хэш транзакции
-# Можно посмотреть статус тут https://testnet.bscscan.com/
-hash = txn_hash.hex()
-txn = web3.eth.get_transaction_receipt(hash)
-logger.info(txn)
-
-
-# check_balance(get_wallet())
+# send_txn(account_from)
+depositETH()
--- TEST.py
+++ TEST.py
@@ -11,18 +11,18 @@ import struct
 # !Начать следующий раз с подключения кошелька к сату --> потом уже запрос пост на получение токенов --> потом отправка транз на контракт
 
 
-# private_key = "1effc9349ce5481f0cdf0fb07e8845be6051ee58c57d6e95e6b90bfb8f55a964"
-# account_from = "0x04Ee5860e4fce5560865197BCfb83b9192ce4dbD"
-# account_contract = "0x47c02b023b6787ef4e503df42bbb1a94f451a1c0"
+private_key = "1effc9349ce5481f0cdf0fb07e8845be6051ee58c57d6e95e6b90bfb8f55a964"
+account_from = "0x04Ee5860e4fce5560865197BCfb83b9192ce4dbD"
+account_contract = "0x47c02b023b6787ef4e503df42bbb1a94f451a1c0"
 
 
-# scroll_l1 = Web3.HTTPProvider('https://prealpha-rpc.scroll.io/l1')
-# scroll_l2 = Web3.HTTPProvider('https://prealpha-rpc.scroll.io/l2')
+scroll_alpha = Web3.HTTPProvider('https://alpha-rpc.scroll.io/l2')
+goerly = Web3.HTTPProvider(
+    'https://endpoints.omniatech.io/v1/eth/goerli/public')
 
-# web3 = Web3(scroll_l1)
-# logger.info(f"Is connected: {web3.isConnected()}")
-# web3.middleware_onion.inject(geth_poa_middleware, layer=0)
-
+web3 = Web3(goerly)
+logger.info(f"Is connected: {web3.isConnected()}")
+web3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 # def send_txn(account_from, account_contract):
 
@@ -40,13 +40,10 @@ import struct
 #     }
 #     return txn
 
-
 # transaction = send_txn(account_from, account_contract)
-
 
 # # 2. Подписываем транзакцию с приватным ключом
 # signed_txn = web3.eth.account.sign_transaction(transaction, private_key)
-
 
 # # 3. Отправка транзакции
 # txn_hash = web3.eth.sendRawTransaction(signed_txn.rawTransaction)
@@ -57,35 +54,22 @@ import struct
 # txn = web3.eth.get_transaction_receipt(hash)
 # logger.info(txn)
 
+# def get_response(url, method='POST'):
+#     keys = {"address": "0x7A8463B955c403ebd4611b18743a40aeBa0d7d4F",
+#             "h-captcha-response": "P1_eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.hKdwYXNza2V5xQQNHAnkIVwbnlu76Xyvp6Zrjp-TfJAHFKtQiO5CG3zydkHaEPjTkbDNkz3GgkqdEUqhalHsRnRQZuQ_-RoIOKBmtJNSgExqUzIj9jPlOUWT3xNQlk9LTO-fNRYa52Y0f6NqvpcBkEXkwMAhrkhBDfoiExLT7h3Qhx39s6kk5Gg8tB2IkVO08otdHdqJBWgIKeHWEaXrEL_uiUR41fqCtcc9tAYZ_ZHSDpQHZuJciejWPLSUVNPrDMMwmHXpj3MjqgXDv5vGVOyL3-iTf4RlOP6gwr6R3Y6vq0ccoF-XJwA6QIJhIE99RX35tosf9MyrL338EhyVQ_cTyCUp7BzITotTthJjkraz-hxQp6VUzJOrmsan9NqKRyNsyQvDq8i-HMvPx9n7zQpUcOfMcbp4Ka-HttYA_xp8lJTWUvCptexvIlu-QNT1f_rH5lTRn02BiEaKOr5rIe_nyVPdw67RhF78SQ8IO-cQQLN2-5bpf87o4UR-8U4IP_MlF14SdaWA-HQzTJvoPyKbSUKggbCODK_fnjRGuJSp7iT2dCx3eEzYGnjrUq4GNE8gNj1rq6OZmXuwvF8E54UOWhLuBVVu-ZCe38ffn6wPvIP-6FQBw2LdLOTsSbPpR1y3WmSOcnLyN-8_0gjINYVhc8OJhLq9plVtQcX5sJWtZRhBBvb0tTHeNdB_awOBJ9BzpnObEyXAMkJExoV1ujB0kf9lnagcWFBSAbwx2uYiAnvTDg-wPHY_1KH1qIJuKYVOIEHfjJYUGLpX7Xqs4pI3rbTiCtW9xvIlZRfayf-Gz1yyOJAICDXMCeHxMS3Q7L1VSEpnWJcW--MCrzKS5QfPjqxIC3uKTpM3BqSU0cpeUXRQ8uvc_paZBV2xnX5Vrhl0rWrFWpYwJSv3VwAgD_O_migePNvtnoO4UePS-5pJ5pqwcNPbCpBHwsPsG4-3nBsnFy8X93gaIPHSFTJrqBrZqv-9VZ0AIeuNTYf0qo36SehO-oBomyIDSlj7pbjigTGb5TJAgqkUVBCVfqDIonaGR7MJeFRJ6KPHc92YmDJYb_nhWkMG7A22Xmip4RM6abOezX0g7BgNxYQq4kiOFHiKsW7PeHDE3XXSSLlK5V1ARP8veN6eE3rpzWO4cxi3Eeuvz-wFL-1GBNsjyTqmHFCte3mJw_M-sJkfTgFt0CvfCyy7SGQZH8fX34WZpKbLg6UKA-BaWvTy4urxiEv70WfohdeIXEqFSTQssj_iaYpjF322sVpyzxMCLHRHrkg_dWee4Pa0p7HwRQ01qVc-WGNo4tm4lZGFAU_EzyjTd1Ph8KpfyxXA77FEugEZhQ-UURIm5gNCbJo4Zbgt_-F8idCDAUc996vuWSKFLXI8Xh_IokswVd-ihR-jZXhwzmP8I_uoc2hhcmRfaWTOFZnkVKJwZAA.vvF-uazno6wMya1iwcFp2vErsy7DHV5q4nkbC6UcIns"}
+#     headers = {
+#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
+#         "cookie": "__cfduid=da350b8e01521848041e557b8840722281595350016; PHPSESSID=rgr9ath2l0aumnkbt9s4k41if4"
+#     }
+#     response = requests.post(url, headers=headers, params=keys)
+# #response = requests.request(method, url, headers=headers, timeout=15)
+#     text_response = response.text
+#     return text_response
 
-def get_response(url, method='POST'):
-    keys = {"address": "0x7A8463B955c403ebd4611b18743a40aeBa0d7d4F",
-            "h-captcha-response": "P1_eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.hKdwYXNza2V5xQQNHAnkIVwbnlu76Xyvp6Zrjp-TfJAHFKtQiO5CG3zydkHaEPjTkbDNkz3GgkqdEUqhalHsRnRQZuQ_-RoIOKBmtJNSgExqUzIj9jPlOUWT3xNQlk9LTO-fNRYa52Y0f6NqvpcBkEXkwMAhrkhBDfoiExLT7h3Qhx39s6kk5Gg8tB2IkVO08otdHdqJBWgIKeHWEaXrEL_uiUR41fqCtcc9tAYZ_ZHSDpQHZuJciejWPLSUVNPrDMMwmHXpj3MjqgXDv5vGVOyL3-iTf4RlOP6gwr6R3Y6vq0ccoF-XJwA6QIJhIE99RX35tosf9MyrL338EhyVQ_cTyCUp7BzITotTthJjkraz-hxQp6VUzJOrmsan9NqKRyNsyQvDq8i-HMvPx9n7zQpUcOfMcbp4Ka-HttYA_xp8lJTWUvCptexvIlu-QNT1f_rH5lTRn02BiEaKOr5rIe_nyVPdw67RhF78SQ8IO-cQQLN2-5bpf87o4UR-8U4IP_MlF14SdaWA-HQzTJvoPyKbSUKggbCODK_fnjRGuJSp7iT2dCx3eEzYGnjrUq4GNE8gNj1rq6OZmXuwvF8E54UOWhLuBVVu-ZCe38ffn6wPvIP-6FQBw2LdLOTsSbPpR1y3WmSOcnLyN-8_0gjINYVhc8OJhLq9plVtQcX5sJWtZRhBBvb0tTHeNdB_awOBJ9BzpnObEyXAMkJExoV1ujB0kf9lnagcWFBSAbwx2uYiAnvTDg-wPHY_1KH1qIJuKYVOIEHfjJYUGLpX7Xqs4pI3rbTiCtW9xvIlZRfayf-Gz1yyOJAICDXMCeHxMS3Q7L1VSEpnWJcW--MCrzKS5QfPjqxIC3uKTpM3BqSU0cpeUXRQ8uvc_paZBV2xnX5Vrhl0rWrFWpYwJSv3VwAgD_O_migePNvtnoO4UePS-5pJ5pqwcNPbCpBHwsPsG4-3nBsnFy8X93gaIPHSFTJrqBrZqv-9VZ0AIeuNTYf0qo36SehO-oBomyIDSlj7pbjigTGb5TJAgqkUVBCVfqDIonaGR7MJeFRJ6KPHc92YmDJYb_nhWkMG7A22Xmip4RM6abOezX0g7BgNxYQq4kiOFHiKsW7PeHDE3XXSSLlK5V1ARP8veN6eE3rpzWO4cxi3Eeuvz-wFL-1GBNsjyTqmHFCte3mJw_M-sJkfTgFt0CvfCyy7SGQZH8fX34WZpKbLg6UKA-BaWvTy4urxiEv70WfohdeIXEqFSTQssj_iaYpjF322sVpyzxMCLHRHrkg_dWee4Pa0p7HwRQ01qVc-WGNo4tm4lZGFAU_EzyjTd1Ph8KpfyxXA77FEugEZhQ-UURIm5gNCbJo4Zbgt_-F8idCDAUc996vuWSKFLXI8Xh_IokswVd-ihR-jZXhwzmP8I_uoc2hhcmRfaWTOFZnkVKJwZAA.vvF-uazno6wMya1iwcFp2vErsy7DHV5q4nkbC6UcIns"}
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win32; x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
-        "cookie": "__cfduid=da350b8e01521848041e557b8840722281595350016; PHPSESSID=rgr9ath2l0aumnkbt9s4k41if4"
-    }
-    response = requests.post(url, headers=headers, params=keys)
-#response = requests.request(method, url, headers=headers, timeout=15)
-    text_response = response.text
-    return text_response
-
-
-while True:
-    res = get_response("https://prealpha-api.scroll.io/faucet/api/claim")
-    text_response = res
-    parse_data = BeautifulSoup(res, features="lxml")
-    out = parse_data.find('button', {'class': 'MuiButtonBase-root'})
-    # if "Request tokens every 24h." in text_response:
-    #     print('Max claims lets wait for a day')
-    #     time.sleep(86400)
-    #     continue
-    # if out is not None:
-    #     print('Logged out')
-    #     res = get_response(
-    #         "https://faucetbch.com/index.php?login=true&email=qqxfgklk4el69e5u45wphqm245k3t5rhpcwac7uhwe")
-    #     res = get_response("https://faucetbch.com/index.php?a=faucet2")
-    #     print(res)
-    #     time.sleep(2)
-    print(res)
-    break
+# while True:
+#     res = get_response("https://prealpha-api.scroll.io/faucet/api/claim")
+#     text_response = res
+#     parse_data = BeautifulSoup(res, features="lxml")
+#     out = parse_data.find('button', {'class': 'MuiButtonBase-root'})
+#     print(res)
+#     break
